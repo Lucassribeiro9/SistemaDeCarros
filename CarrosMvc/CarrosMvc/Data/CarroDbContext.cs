f61b5f35640d769a89720c1b7ac79cddcbf57179
Lucassribeiro9 <lucas.devlop94@gmail.com>
2023-07-28 17:21:52 -0300

    criacao dbcontext

---
+++ CarrosMvc/CarrosMvc/Data/CarroDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarrosMvc.Models;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+public class CarroDbContext : DbContext
+{
+    public CarroDbContext(DbContextOptions<CarroDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Carro> Carros { get; set; } = default!;
+    public DbSet<CarroFlex> CarrosFlex { get; set; }
+    public DbSet<CarroDiesel> CarrosDiesel { get; set; }
+    public DbSet<CarroEletrico> CarrosEletricos { get; set; }
+}
